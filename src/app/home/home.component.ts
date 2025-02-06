3a193be3e996620c79f04ef6783fd711203fb2a3
Gianluca <gianlucadl92@gmail.com>
2025-02-06 14:02:17 +0100

    recupe tutti i dati

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CocktailService } from '../service/cocktail.service';
 
@@ -8,23 +8,31 @@ import { CocktailService } from '../service/cocktail.service';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   cocktails: any[] = [];
   searchQuery: string = '';
 
   constructor(private cocktailService: CocktailService) {}
 
-  searchCocktails(event: Event): void {
+  ngOnInit() {
+    //Carica i cocktail automaticamente all'avvio
+    this.cocktailService.getAllCocktails().subscribe((data: any) => {
+      this.cocktails = data.drinks || []; // Se "drinks" è undefined, usa un array vuoto
+    });
+  }
+
+  searchCocktails(event: Event) {
     const input = event.target as HTMLInputElement;
     this.searchQuery = input.value;
 
     if (this.searchQuery.trim() === '') {
-      this.cocktails = [];
-      return;
+      //Se il campo di ricerca è vuoto, ricarica tutti i cocktail
+      this.ngOnInit();
+    } else {
+      //Altrimenti, esegui la ricerca
+      this.cocktailService.searchCocktails(this.searchQuery).subscribe((data: any) => {
+        this.cocktails = data.drinks || [];
+      });
     }
-
-    this.cocktailService.getCocktails(this.searchQuery).subscribe((data) => {
-      this.cocktails = data.drinks || [];
-    });
   }
 }
--- src/app/service/cocktail.service.ts
+++ src/app/service/cocktail.service.ts
@@ -1,16 +1,22 @@
 import { HttpClient } from '@angular/common/http';
-import { Inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CocktailService {
-  private apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+  private apiURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
   constructor(private http: HttpClient) {}
 
-  getCocktails(searchTerm: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}${searchTerm}`);
+  //Metodo per caricare tutti i cocktail (ad esempio mostrando quelli con "a" iniziale)
+  getAllCocktails(): Observable<any> {
+    return this.http.get(this.apiURL + 'a'); // Prende tutti i cocktail che contengono "a"
+  }
+
+  //Metodo per cercare cocktail specifici
+  searchCocktails(query: string): Observable<any> {
+    return this.http.get(this.apiURL + query);
   }
 }
