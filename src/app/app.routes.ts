941efe5f349c8780b5a22dc50795eca6239a1aaa
Gianluca <gianlucadl92@gmail.com>
2025-02-10 12:09:20 +0100

    fix design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,10 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { ClientiComponent } from './clienti/clienti.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'clienti', component: ClientiComponent },
   { path: '**', redirectTo: '' },
 ];
